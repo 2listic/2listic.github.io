f2aca5c196dda28b5acb9242127c7edc0441788d
mfranzon <franzonmarco94@gmail.com>
2024-03-19 10:00:20 +0100

    add publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://dualistic.it",
   integrations: [tailwind(), mdx(), sitemap()],
 });
