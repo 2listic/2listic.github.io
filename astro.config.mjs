21507a84f0671ea30d9806cbac925cd07f178d38
Pablo Colturi <pcolturi@proton.me>
2025-10-29 11:31:56 +0100

    update astro-icon v1 + team experience grid + linkedin in contact

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,20 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://dualistic.it",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon({
+      include: {
+        uil: ["*"], // Include all Unicons
+      },
+    })
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
